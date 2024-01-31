ce19aca8fa56cea7dad7bb72c71263947d6f7401
shivanshu0nodejs <145346118+shivanshu0nodejs@users.noreply.github.com>
2024-01-31 16:19:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,23 +3,25 @@ require("dotenv").config();
 const app = express();
 const connectDB = require("./db/connect.js");
 const routes = require("./routes/route.js");
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3600;
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/", routes);
 
+app.use('/uploads/documents', express.static('uploads/documents'));
+app.use('/uploads/profileImage', express.static('uploads/profileImage'));
+
 const start = async() => {
-
-    try{
-
+    try {
         await connectDB(process.env.MONGODB_URI);
         app.listen(PORT, () => {
             console.log(`Connected To PORT ${PORT}`);
         });
-
-    }catch (error){
+    } catch (error) {
         console.log(error);
     }
 };
